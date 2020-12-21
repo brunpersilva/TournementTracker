4dcc67eef1ec1b524d265f7cf0bb5f9e6806a40e
Bruno Silva <brunpersilva@gmail.com>
2020-12-21 13:12:41 -0300

    Created folders for entities and connections classes in library project

---
+++ TrackerLibrary/Connetions/GlobalConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Connections
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connection { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializedConnections(bool database, bool textFile)
+        {
+            if (database)
+            {
+                // TODO - Set up the sql properly
+                SqlConnector sql = new SqlConnector();
+                Connection.Add(sql);
+            }
+
+            if (textFile)
+            {
+                // TODO - CREATE TEXT CONNECTION
+                TextConnection text = new TextConnection();
+                Connection.Add(text);
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/Connetions/IDataConnection.cs
@@ -0,0 +1,9 @@
+﻿using TrackerLibrary.Entities;
+
+namespace TrackerLibrary.Connections
+{
+    public interface IDataConnection
+    {
+        PrizeModel CreatePrize(PrizeModel model);
+    }
+}
